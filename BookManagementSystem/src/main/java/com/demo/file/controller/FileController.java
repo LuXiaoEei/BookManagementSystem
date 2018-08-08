bc6bcd12fcb5f1c4ae2de8d041d6ed9eb52a1154
LuXiaoEei <438719297@qq.com>
2018-08-08 21:22:03 +0800

    add the function about File
    add the function about File, including add, describe, selectbybookname, selectbyuser

--- BookManagementSystem/src/main/java/com/demo/file/controller/FileController.java
+++ BookManagementSystem/src/main/java/com/demo/file/controller/FileController.java
@@ -1,30 +1,82 @@
 package com.demo.file.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.demo.file.model.BookFile;
+import com.demo.file.repository.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import  java.io.*;
+import java.lang.System;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.Collection;
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/file/book/*" ,method = RequestMethod.GET)
+@RequestMapping(value = "/0" ,method = RequestMethod.GET)
 public class FileController {
+    @Autowired
+    FileRepository fileRepository;
+
+//    FileManager fileManager = new FileManager();
+    private Path rootpath = Paths.get("", "src","main","java","com","demo","file","date");
+    private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+    @ExceptionHandler(Exception.class)
+    public void processMethod(Exception ex, HttpServletRequest request , HttpServletResponse response) throws IOException {
+        System.out.println("error: "+ex.getMessage());
+        response.getWriter().printf("error: "+ex.getMessage());
+        response.flushBuffer();
+    }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String addBook(@RequestParam(value = "isbn", required = true) String isbn,
-                            @RequestParam(value = "bookname", required = true) String bookname,
-                            @RequestParam(value = "category", required = false,defaultValue = "") String category,
-                            @RequestParam(value = "publicationyear", required = false,defaultValue = "") String publicationyear) {
-        return"1";
+    public BookFile addBook(@RequestParam(value = "id", required = true) String id,
+                        @RequestParam(value = "isbn", required = true) String isbn,
+                        @RequestParam(value = "bookname", required = true) String bookname,
+                        @RequestParam(value = "press", required = false,defaultValue = "") String press,
+                        @RequestParam(value = "category", required = false,defaultValue = "") String category,
+                        HttpServletResponse response) throws IOException {
 
+        BookFile bookFile = new BookFile(id,isbn,bookname,category,press,df.format(new Date()),"","","");
+        return fileRepository.save(bookFile);
     }
 
-    @RequestMapping(value = "/describle", method = RequestMethod.GET)
-    public String describleBook(@RequestParam(value = "rows", required = false,defaultValue = "-1") int rows){
-        return "1";
+    @RequestMapping(value = "/describe", method = RequestMethod.GET)
+    public ArrayList<BookFile> describe(@RequestParam(value = "start", required = false,defaultValue = "0") int start,
+                                        @RequestParam(value = "nums", required = false,defaultValue = "all") String nums
+    ) throws IOException {
+        HashMap<Integer,BookFile> allcontent = fileRepository.readFileSource();
+        ArrayList<BookFile> result = new ArrayList<BookFile>();
+        Integer end=((nums.equals("all"))?allcontent.keySet().size():Integer.valueOf(nums))+start;
+        for (Integer i=start;i<end;i++){
+            if (allcontent.get(i)!=null){
+                result.add(allcontent.get(i));
+            }
+        }
+        return result;
     }
+
+    @RequestMapping(value = "/select/user/{user}", method = RequestMethod.GET)
+    public ArrayList<BookFile> findByUser(@PathVariable(required = true) String user) throws IOException {
+        Collection<BookFile> allcontent = fileRepository.readFileSource().values();
+        return allcontent.stream().filter(x->x.getUser().equals(user)).collect(Collectors.toCollection(ArrayList<BookFile>::new));
+    }
+
+    @RequestMapping(value = "/select/bookname/{bookname}", method = RequestMethod.GET)
+    public ArrayList<BookFile> findByBookname(@PathVariable(required = true) String bookname) throws IOException {
+        Collection<BookFile> allcontent = fileRepository.readFileSource().values();
+//        allcontent.forEach(x->System.out.println(x.getBookname()));
+        return allcontent.stream().filter(x->x.getBookname().contains(bookname)).collect(Collectors.toCollection(ArrayList<BookFile>::new));
+    }
+
+
+
+
+
 }
 
--- BookManagementSystem/src/main/java/com/demo/file/repository/FileRepository.java
+++ BookManagementSystem/src/main/java/com/demo/file/repository/FileRepository.java
@@ -1,4 +1,62 @@
 package com.demo.file.repository;
 
+import com.demo.file.model.BookFile;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
+
+import java.awt.print.Book;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+
+@Repository
 public class FileRepository {
+
+    private Path filepath = Paths.get("", "src","main","java","com","demo","file","date","filesource.txt");
+
+    public HashMap<Integer,BookFile> readFileSource() throws IOException {
+        HashMap<Integer,BookFile> allcontent= new HashMap<Integer,BookFile>();
+        BufferedReader br = new BufferedReader( new FileReader(filepath.toString()));
+        br.readLine();
+        int id = 0;
+        while (true) {
+            String tmp=br.readLine();
+            if (tmp!=null){
+//                tmp=tmp+"\tw";
+                BookFile bookFile= new BookFile(tmp.split("\t"));
+                allcontent.put(id,bookFile);
+                id++;
+            }else{
+                break;
+            }
+        }
+        return allcontent;
+    }
+
+    public BookFile save(BookFile bookFile) throws IOException {
+        File file = new File(filepath.toString());
+        if (!file.exists()){
+            FileWriter newfile = new FileWriter(filepath.toString());
+            newfile.write("isbn\tbookname\tpress\tcategory\tupdatetime\tuser\tloantime\treturntime\tid\r\n");
+            newfile.close();
+        }
+        FileWriter filesource = new FileWriter(filepath.toString(),true);
+        filesource.write(bookFile.toString());
+        filesource.close();
+        return bookFile;
+    }
+
+
+
+
+
+
+
+
 }
