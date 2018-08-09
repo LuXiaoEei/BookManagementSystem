f6c847575ffecfd5955c5941cd55585c91dca8d3
AP\LLi214 <LLi214@its.jnj.com>
2018-08-09 19:25:34 +0800

    客户端上传文件到服务端

--- client_test/src/main/java/demo/RunnableDemo.java
+++ client_test/src/main/java/demo/RunnableDemo.java
@@ -1,7 +1,9 @@
 package demo;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
+import java.lang.*;
+import java.util.*;
 import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,7 +16,6 @@ public class RunnableDemo implements Runnable {
 
     public RunnableDemo() throws IOException,NullPointerException {
     }
-
     @Override
     public void run() {
        /* while (reader.ready()) {
@@ -26,48 +27,52 @@ public class RunnableDemo implements Runnable {
             // 转成char加到StringBuffer对象中
         }
         System.out.println(sb.toString());*/
-       try {
-           String s = null;
-           RestTemplate restTemplate = new RestTemplate();
+        try {
+            String s = null;
+            RestTemplate restTemplate = new RestTemplate();
           /*synchronized (br){
                while ((s = br.readLine())!= null) {//使用readLine方法，一次读一行
                    System.out.println(Thread.currentThread()+"  "+s);
                    br.wait(100);
                }}*/
-           while ((s = br.readLine())!= null) {//使用readLine方法，一次读一行
-               if(s.length()==0)  {break;}
-               String str=null;
-               String w=null;
-              // String regEx="[`~@#$%^*()=|{}';'\\[\\]</?~@#￥%……*（）——|{}【】\"*‘；”“’    。、？]";
+            while ((s = br.readLine())!= null) {//使用readLine方法，一次读一行
+                if(s.length()==0)  {break;}
+                String str=null;
+                String w=null;
+                // String regEx="[`~@#$%^*()=|{}';'\\[\\]</?~@#￥%……*（）——|{}【】\"*‘；”“’    。、？]";
                /*Pattern p = Pattern.compile(regEx);
                Matcher m = p.matcher(s);
                 str= m.replaceAll("").trim();*/
-               //System.out.println(Thread.currentThread()+": "+str);
-                   String a [] = s.split("\t");
-                   if (a[0].equals("bookName")) {break;}
-                   for(int i=0;i<a.length;i++){
-                  // String regEx= "^(\\?\\s{3}?)|(\")|(\\?)";
+                //System.out.println(Thread.currentThread()+": "+str);
+                String a[] = s.split("\t");
+                if (a[0].equals("bookName")) {break;}
+                for(int i=0;i<a.length;i++){
+                    String item = a[i].replaceAll("\\?","");
+                    item = item.replaceAll("\"","");
+                    item =item.trim();
+                    a[i]=item;
+                 /* String regEx= "^(\\?\\s{3}?)|(\")|(\\?)";
                        String regEx= "^[^\\w]+|[^[\\w\\+\\!]+]$";
                    Pattern p = Pattern.compile(regEx);
                    Matcher m = p.matcher(a[i]);
-                   a[i]= m.replaceAll("").trim();
-                       System.out.println(a[i]);
+                   a[i]= m.replaceAll("").trim();*/
+                    System.out.println(a[i]);
                       /* String regE= "\\?$";
                        Pattern q = Pattern.compile(regE);
                        Matcher n = q.matcher(e);
                        a[i]= n.replaceAll("").trim();*/
-               }
+                }
 
-                /*  w=(a[2].length()==14)?(a[2].replace("-","")):(a[2].length()==0?(978+a[3]):a[2]);
+                 /* w=(a[2].length()==14)?(a[2].replace("-","")):(a[2].length()==0?(978+a[3]):a[2]);
                   String url = "http://localhost:8080/2/add?bookname="+a[0]+"&press="+a[1]+"&isbn="+w+"&category="+a[4];
                    ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);*/
-                  //System.out.println(Thread.currentThread()+": "+ Arrays.toString(a));
-                 //br.wait(100);
-           }
-       }
-       catch (IOException e){
-           e.printStackTrace();
-       } /*catch (InterruptedException e) {
+                //System.out.println(Thread.currentThread()+": "+ Arrays.toString(a));
+                //br.wait(100);
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        } /*catch (InterruptedException e) {
            e.printStackTrace();
        }*/
     }
