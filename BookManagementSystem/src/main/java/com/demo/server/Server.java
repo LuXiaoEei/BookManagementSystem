a4554809f08fdc295e878c6df16d4c071ff69f5c
LuXiaoEei <438719297@qq.com>
2018-08-10 11:10:25 +0800

    add frame

--- BookManagementSystem/src/main/java/com/demo/server/Server.java
+++ BookManagementSystem/src/main/java/com/demo/server/Server.java
@@ -2,13 +2,14 @@ package com.demo.server;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.function.Predicate;
 
 public interface Server {
     public String addbook(String id , String bookname, String isbn, String  category, String press, String user, String loantime, String returntime, String updatetime,HttpServletResponse response) throws IOException;
 
     public Object deleteBookByIsbn(String isbn,HttpServletResponse response);
 
-    public Object updateBookByIsbn(String isbn,HttpServletResponse response);
+    public Object updateBookByIsbn(String press, String category, String bookname, String isbn, String condition, HttpServletResponse response);
 
     public Object describeBook(int starts,int num,HttpServletResponse response);
 
