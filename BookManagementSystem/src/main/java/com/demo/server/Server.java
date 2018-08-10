3157378c2a0cc3bbf66316593d115d21400df9e6
AP\LLi214 <LLi214@its.jnj.com>
2018-08-10 15:42:39 +0800

    Server.java

--- BookManagementSystem/src/main/java/com/demo/server/Server.java
+++ BookManagementSystem/src/main/java/com/demo/server/Server.java
@@ -1,26 +1,28 @@
 package com.demo.server;
 
 import com.demo.error.BooknameNotFound;
+import com.demo.error.IdError;
 import com.demo.error.IsbnNotFound;
+import com.demo.file.model.BookFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.function.Predicate;
+import java.text.ParseException;
 
 public interface Server {
-    public String addbook(String id , String bookname, String isbn, String  category, String press, String user, String loantime, String returntime, String updatetime,HttpServletResponse response) throws IOException;
+    public BookFile addbook(String id , String bookname, String isbn, String  category, String press, String user, String loantime, String returntime, String updatetime, HttpServletResponse response) throws IOException, IdError, IsbnNotFound;
 
-    public Object deleteBookByIsbn(String isbn,HttpServletResponse response);
+    public Object deleteBookByIsbn(String isbn,HttpServletResponse response) throws IOException;
 
-    public Object updateBookByIsbn(String press, String category, String bookname, String isbn, String condition, HttpServletResponse response) throws IsbnNotFound, BooknameNotFound;
+    public Object updateBookByIsbn(String press, String category, String bookname, String isbn, String condition, HttpServletResponse response) throws IsbnNotFound, BooknameNotFound, IOException;
 
-    public Object describeBook(String start, String nums,HttpServletResponse response);
+    public Object describeBook(String start, String nums,HttpServletResponse response) throws IOException;
 
-    public Object selectByBooknameLike(String bookname,HttpServletResponse response);
+    public Object selectByBooknameLike(String bookname,HttpServletResponse response) throws IOException;
 
-    public Object selectByUser(String user,HttpServletResponse response);
+    public Object selectByUser(String user,HttpServletResponse response) throws IOException;
 
-    public void loanBookByUserAndIsbn(String user,String isbn,HttpServletResponse response) throws IOException;
+    public void loanBookByUserAndIsbn(String user,String isbn,HttpServletResponse response) throws IOException, ParseException;
 
     public Object returnbookByUserAndIsbn(String user,String isbn,HttpServletResponse response) throws IOException;
 
