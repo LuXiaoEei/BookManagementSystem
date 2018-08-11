ab17b9d24c87ffad6b32eb502a84ff63c234f1c4
AP\LLi214 <LLi214@its.jnj.com>
2018-08-11 11:38:29 +0800

    url转换

---
+++ client_test/src/main/java/demo/RunnableDemo2.java
@@ -0,0 +1,55 @@
+package demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.*;
+
+public class RunnableDemo2 implements Runnable {
+    private String s = "C:\\Users\\lli214\\Desktop\\mockUpDataForInterns.txt";
+    BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(s)), "GBK"));//构造一个BufferedReader类来读取文件
+
+    public RunnableDemo2() throws IOException, NullPointerException {
+    }
+
+    @Override
+    public void run() {
+        try {
+            String s = null;
+            RestTemplate restTemplate = new RestTemplate();
+            while ((s = br.readLine()) != null) {//使用readLine方法，一次读一行
+                if (s.length() == 0) {
+                    break;
+                }
+                String str = null;
+                String a[] = s.split("\t");
+                if (a[0].equals("bookName")) {break;}
+                //处理字符
+                for(int i=0;i<a.length;i++){
+                    String item = a[i].replaceAll("\\?","");
+                    item = item.replaceAll("\"","");
+                    item =item.trim();
+                    a[i]=item;}
+                    //合并ISBN和ASIN
+                String isbn=(a[2].length()==14)?(a[2].replace("-","")):(a[2].length()==0?(978+a[3]):a[2]);
+                   //传url
+                String url =  "http://localhost:8080/add";
+                MultiValueMap<String,String> data = new LinkedMultiValueMap<String,String>();
+                data.add("id","1");
+                data.add("bookname",a[0]);
+                data.add("press",a[1]);
+                data.add("isbn",isbn);
+                data.add("category",a[4]);
+                ResponseEntity<String> response = restTemplate.postForEntity(url,data,String.class);
+                System.out.println(response.toString());
+                System.out.println(response.getStatusCode().equals(HttpStatus.OK));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- client_test/src/main/java/demo/Threadtest.java
+++ client_test/src/main/java/demo/Threadtest.java
@@ -3,7 +3,7 @@ package demo;
 public class Threadtest {
     public static void main(String []args) throws Exception {
 
-        RunnableDemo r1=new RunnableDemo();
+        /*RunnableDemo r1=new RunnableDemo();
         Thread t1=new Thread(r1) ;
         t1.start();
         RunnableDemo r2=new RunnableDemo();
@@ -11,6 +11,15 @@ public class Threadtest {
         t2.start();
         RunnableDemo r3=new RunnableDemo();
         Thread t3=new Thread(r1) ;
+        t3.start();*/
+        RunnableDemo2 r=new RunnableDemo2();
+        Thread t1=new Thread(r) ;
+        t1.start();
+        // RunnableDemo2 r2=new RunnableDemo2();
+        Thread t2=new Thread(r) ;
+        t2.start();
+        //RunnableDemo2 r3=new RunnableDemo2();
+        Thread t3=new Thread(r) ;
         t3.start();
     }
 }
