962521f253c8c92f0a7db4a8c8a1c650bc4ebfa7
AP\LLi214 <LLi214@its.jnj.com>
2018-08-11 19:38:47 +0800

    客户端更改

--- client_test/src/main/java/demo/RunnableDemo.java
+++ client_test/src/main/java/demo/RunnableDemo.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
 import java.lang.*;
 import java.util.*;
 import java.io.*;
@@ -12,25 +13,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RunnableDemo implements Runnable {
-    public static int cout=1;
+    public static int id = 1;
     private String s = "C:\\Users\\lli214\\Desktop\\mockUpDataForInterns.txt";
-    //InputStreamReader reader = new InputStreamReader(new FileInputStream(new File(s)), "GBK");
-    //StringBuffer sb = new StringBuffer();
     BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(s)), "GBK"));//构造一个BufferedReader类来读取文件
 
-    public RunnableDemo() throws IOException,NullPointerException {
+    public RunnableDemo() throws IOException, NullPointerException {
     }
+
     @Override
     public void run() {
-       /* while (reader.ready()) {
-            try {
-                sb.append((char) reader.read());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            // 转成char加到StringBuffer对象中
-        }
-        System.out.println(sb.toString());*/
         try {
             String s = null;
             RestTemplate restTemplate = new RestTemplate();
@@ -39,51 +30,41 @@ public class RunnableDemo implements Runnable {
                     if (s.length() == 0) {
                         break;
                     }
-                    // String regEx="[`~@#$%^*()=|{}';'\\[\\]</?~@#￥%……*（）——|{}【】\"*‘；”“’    。、？]";
-               /*Pattern p = Pattern.compile(regEx);
-               Matcher m = p.matcher(s);
-                str= m.replaceAll("").trim();*/
-                    //System.out.println(Thread.currentThread()+": "+str);
-                  /* String regEx= "^(\\?\\s{3}?)|(\")|(\\?)";
-                     String regEx= "^[^\\w]+|[^[\\w\\+\\!]+]$";
-                   Pattern p = Pattern.compile(regEx);
-                   Matcher m = p.matcher(a[i]);
-                   a[i]= m.replaceAll("").trim();*/
                     String a[] = s.split("\t");
                     if (a[0].equals("bookName")) {
                         break;
                     }
                     for (int i = 0; i < a.length; i++) {
+                        // a[i]= a[i].replaceAll("\\?", "").replaceAll("\"", "").trim();
                         String item = a[i].replaceAll("\\?", "");
                         item = item.replaceAll("\"", "");
                         item = item.trim();
                         a[i] = item;
-                        System.out.println(a[i]);
+                       // System.out.println(a[i]);
                     }
-                    String isbn=(a[2].length()==14)?(a[2].replace("-","")):(a[2].length()==0?(978+a[3]):a[2]);
-                    //传url
-                    String url =  "http://localhost:8080/add";
-                    MultiValueMap<String,String> data = new LinkedMultiValueMap<>();
-                    data.add("id",String.valueOf(cout++));
-                    data.add("bookname",a[0]);
-                    data.add("press",a[1]);
-                    data.add("isbn",isbn);
-                    data.add("category",a[4]);
-                    ResponseEntity<String> response = restTemplate.postForEntity(url,data,String.class);
+                    String isbn = (a[2].length() == 14) ? (a[2].replace("-", "")) : (a[2].length() == 0 ? (978 + a[3]) : a[2]);
+
+                    String url = "http://localhost:8080/add";
+                    MultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+                    data.add("id", String.valueOf(id++));
+                    data.add("bookname", a[0]);
+                    data.add("press", a[1]);
+                    data.add("isbn", isbn);
+                    data.add("category", a[4]);
+                    ResponseEntity<String> response = restTemplate.postForEntity(url, data, String.class);
                     System.out.println(response.toString());
                     System.out.println(response.getStatusCode().equals(HttpStatus.OK));
-                   // w = (a[2].length() == 14) ? (a[2].replace("-", "")) : (a[2].length() == 0 ? (978 + a[3]) : a[2]);
+                    // w = (a[2].length() == 14) ? (a[2].replace("-", "")) : (a[2].length() == 0 ? (978 + a[3]) : a[2]);
                     //String url = "http://localhost:8080/2/add?bookname=" + a[0] + "&press=" + a[1] + "&isbn=" + w + "&category=" + a[4];
-                   // ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+                    // ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
                     //System.out.println(Thread.currentThread()+": "+ Arrays.toString(a));
                     br.wait(100);
                 }
             }
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
-           e.printStackTrace();
-       }
+            e.printStackTrace();
+        }
     }
 }
--- client_test/src/main/java/demo/RunnableDemo2.java
+++ client_test/src/main/java/demo/RunnableDemo2.java
@@ -9,11 +9,13 @@ import org.springframework.web.client.RestTemplate;
 import java.io.*;
 
 public class RunnableDemo2 implements Runnable {
-    public static int cout=1;
+    public static int cout = 1;
     private String s = "C:\\Users\\lli214\\Desktop\\mockUpDataForInterns.txt";
     BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(s)), "GBK"));//构造一个BufferedReader类来读取文件
+
     public RunnableDemo2() throws IOException, NullPointerException {
     }
+
     @Override
     public void run() {
         try {
@@ -24,24 +26,27 @@ public class RunnableDemo2 implements Runnable {
                     break;
                 }
                 String a[] = s.split("\t");
-                if (a[0].equals("bookName")) {break;}
+                if (a[0].equals("bookName")) {
+                    break;
+                }
                 //处理字符
-                for(int i=0;i<a.length;i++){
-                    String item = a[i].replaceAll("\\?","");
-                    item = item.replaceAll("\"","");
-                    item =item.trim();
-                    a[i]=item;}
-                    //合并ISBN和ASIN
-                String isbn=(a[2].length()==14)?(a[2].replace("-","")):(a[2].length()==0?(978+a[3]):a[2]);
-                   //传url
-                String url =  "http://localhost:8080/add";
-                MultiValueMap<String,String> data = new LinkedMultiValueMap<>();
-                data.add("id",String.valueOf(cout++));
-                data.add("bookname",a[0]);
-                data.add("press",a[1]);
-                data.add("isbn",isbn);
-                data.add("category",a[4]);
-                ResponseEntity<String> response = restTemplate.postForEntity(url,data,String.class);
+                for (int i = 0; i < a.length; i++) {
+                    String item = a[i].replaceAll("\\?", "");
+                    item = item.replaceAll("\"", "");
+                    item = item.trim();
+                    a[i] = item;
+                }
+
+                String isbn = (a[2].length() == 14) ? (a[2].replace("-", "")) : (a[2].length() == 0 ? (978 + a[3]) : a[2]);
+                //传url
+                String url = "http://localhost:8080/add";
+                MultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+                data.add("id", String.valueOf(cout++));
+                data.add("bookname", a[0]);
+                data.add("press", a[1]);
+                data.add("isbn", isbn);
+                data.add("category", a[4]);
+                ResponseEntity<String> response = restTemplate.postForEntity(url, data, String.class);
                 System.out.println(response.toString());
                 System.out.println(response.getStatusCode().equals(HttpStatus.OK));
             }
