a5432e23caf3db36e9817a81f6ce9ae3c6939396
AP\LLi214 <LLi214@its.jnj.com>
2018-08-11 12:00:15 +0800

    url转换

--- client_test/src/main/java/demo/Threadtest.java
+++ client_test/src/main/java/demo/Threadtest.java
@@ -2,24 +2,13 @@ package demo;
 
 public class Threadtest {
     public static void main(String []args) throws Exception {
-
-        /*RunnableDemo r1=new RunnableDemo();
-        Thread t1=new Thread(r1) ;
-        t1.start();
-        RunnableDemo r2=new RunnableDemo();
-        Thread t2=new Thread(r1) ;
-        t2.start();
-        RunnableDemo r3=new RunnableDemo();
-        Thread t3=new Thread(r1) ;
-        t3.start();*/
+        //RunnableDemo r=new RunnableDemo();
         RunnableDemo2 r=new RunnableDemo2();
         Thread t1=new Thread(r) ;
-        t1.start();
-        // RunnableDemo2 r2=new RunnableDemo2();
         Thread t2=new Thread(r) ;
-        t2.start();
-        //RunnableDemo2 r3=new RunnableDemo2();
         Thread t3=new Thread(r) ;
+        t1.start();
+        t2.start();
         t3.start();
     }
 }
